91c48926cbe025ce925274506e50bbfa1d6320f9
Action2theFuture <dnstks0204@google.com>
2021-05-26 16:53:34 +0900

    model

---
+++ reservation/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Reservation(models.Model):
+    checkin  = models.DateTimeField()
+    checkout = models.DateTimeField()
+    status   = models.ForeignKey("Status", on_delete=models.CASCADE)
+    user     = models.ForeignKey("user.User", on_delete=models.CASCADE)
+    room     = models.ForeignKey("room.Room", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "reservations"
+
+class Status(models.Model):
+    status = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = "status"
---
+++ room/models.py
@@ -0,0 +1,68 @@
+import datetime
+from django.db import models
+
+class Room(models.Model):
+    name         = models.CharField(max_length=128)
+    min_date     = models.CharField(max_length=32, default="1")
+    city         = models.CharField(max_length=32)
+    capacity     = models.CharField(max_length=32, default="2")
+    latitude     = models.DecimalField(max_digits=9, decimal_places=7)
+    longtitude   = models.DecimalField(max_digits=10, decimal_places=7)
+    is_refund    = models.BooleanField(default=False)
+    price        = models.DecimalField(max_digits=10, decimal_places=2)
+    host         = models.ForeignKey("user.Host", on_delete=models.CASCADE, related_name="room")
+    category     = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="room")
+    able_time    = models.ForeignKey("AbleTime", on_delete=models.CASCADE, related_name="room")
+    disable_date = models.ForeignKey("DisableDate", on_delete=models.CASCADE, related_name="room")
+    amenity      = models.ManyToManyField("Amenity", through="RoomAmenity", related_name="room")
+
+    class Meta:
+        db_table = "rooms"
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = "categories"
+        
+class RoomAmenity(models.Model):
+    room    = models.ForeignKey("Room", on_delete=models.CASCADE)
+    amenity = models.ForeignKey("Amenity", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "room_amenities"
+
+class Image(models.Model):
+    url  = models.CharField(max_length=2000)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE, related_name="image")
+
+    class Meta:
+        db_table = "images"
+
+class Amenity(models.Model):
+    name  = models.CharField(max_length=128)
+    image = models.CharField(max_length=2000)
+
+    class Meta:
+        db_table = "amentities"
+
+class WishList(models.Model):
+    wish_user = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="user_wish")
+    wish_room = models.ForeignKey("Room", on_delete=models.CASCADE, related_name="room_wish")
+
+    class Meta:
+        db_table = "wishlists"
+
+class DisableDate(models.Model):
+    start_date = models.DateField()
+    end_date   = models.DateField()
+
+    class Meta:
+        db_table = "disalbe_dates"
+
+class AbleTime(models.Model):
+    checkin  = models.TimeField(default=datetime.time(15))
+    checkout = models.TimeField(default=datetime.time(11))
+
+    class Meta:
+        db_table = "able_times"
