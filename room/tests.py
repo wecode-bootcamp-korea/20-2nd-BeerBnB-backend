c742c05a978238b7719b9e25b9a31fc34b64ebd5
Mac <mac@Macui-MacBook-Pro.local>
2021-05-27 15:42:50 +0900

    ADD: Room DetailView

--- room/tests.py
+++ room/tests.py
@@ -1,3 +1,156 @@
-from django.test import TestCase
+from django.test      import TestCase, Client
 
-# Create your tests here.
+from room.models      import Room, Category, RoomAmenity, Image, Amenity, WishList, DisableDate, AbleTime
+from user.models      import User,Host, Review
+
+class RoomDetailTest(TestCase):
+    def setUp(self):
+        category=Category.objects.create(
+                id   = 1,
+                name = '아파트'
+            )
+        user=User.objects.create(
+            id         = 1,
+            first_name = '하나',
+            last_name  = '김',
+        )
+        host=Host.objects.create(
+            id       = 1,
+            user     = user,
+            is_super = True
+        )
+        disabledate=DisableDate.objects.create(
+            id         = 1,
+            start_date = '2021-06-03',
+            end_date   = '2021-07-09'
+        )
+        abletime=AbleTime.objects.create(
+            id       = 1,
+            checkin  = '11:00:00',
+            checkout = '13:00:00'
+        )
+        room=Room.objects.create(
+            id           = 1,
+            name         = '양재아파트',
+            city         = '서울 양재동',
+            price        = 100000,
+            category     = category,
+            host         = host,
+            latitude     = 1.0221111,
+            longtitude   = 2.2222211,
+            capacity     = 3,
+            disable_date = disabledate,
+            able_time    = abletime
+        )
+        Image.objects.create(
+            id   = 1,
+            room = room,
+            url  = 'a'
+        )
+        
+        amenity1=Amenity.objects.create(
+            id    = 1,
+            name  = '와인',
+            image = '<i class="fas fa-fan"></i>'
+        )
+
+        amenity2=Amenity.objects.create(
+            id    = 2,
+            name  = '맥주',
+            image = '<i class="fas fa-beer"></i>'
+        )
+
+        RoomAmenity.objects.create(
+            id      = 1,
+            room    = room,
+            amenity = amenity1
+        )
+
+        RoomAmenity.objects.create(
+            id      = 2,
+            room    = room,
+            amenity = amenity2
+        )
+        Review.objects.create(
+            id                = 1,
+            review_user       = user,
+            review_room       = room,
+            cleanliness       = 3.0,
+            communication     = 2.0,
+            checkin           = 4.0,
+            accuracy          = 4.0,
+            location          = 2.0,
+            cost_effectivenes = 4.0
+        )
+
+    def tearDown(self):
+        Room.objects.all().delete()
+        Review.objects.all().delete()
+        Host.objects.all().delete()
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        User.objects.all().delete()
+        Amenity.objects.all().delete()
+        RoomAmenity.objects.all().delete()
+
+    def test_roomdetail_get_view(self):
+        client   = Client()
+        response = client.get('/rooms/1')
+        self.assertEqual(response.json(), {
+            'detail': 
+                {
+                    'room_name': '양재아파트',
+                    'address'  : '서울 양재동',
+                    'price'    : '100000.00',
+                    'room_type': '아파트',
+                    'image'    : 'a',
+                    'is_super' : True,
+                    'host'     : '김하나',
+                    'capacity' : 3,
+                    'amenity'  : [
+                            {
+                            'id'         : 1,
+                            'icon'       : 'fas fa-fan',
+                            'description': '와인'
+                            },
+                            {
+                            'id'         : 2,
+                            'icon'       : 'fas fa-beer',
+                            'description': '맥주'
+                            }
+                        ],
+                    'rating' : [
+                        {
+                        'category'       : 'cleanliness',
+                        'category_rating': 3.0
+                    },
+                    {
+                        'category'       : 'communication',
+                        'category_rating': 2.0
+                    },
+                    {
+                        'category'       : 'checkin',
+                        'category_rating': 4.0
+                    },
+                    {
+                        'category'       : 'accuracy',
+                        'category_rating': 4.0
+                    },
+                    {
+                        'category'       : 'location',
+                        'category_rating': 2.0
+                    },
+                    {
+                        'category'       : 'cost_effectivenes',
+                        'category_rating': 4.0
+                    }
+                    ]
+                }
+        })
+        self.assertEqual(response.status_code, 200)
+        
+    def test_roomdetail_invalid_room_view(self):
+        client   = Client()
+        response = client.get('/rooms/999')
+        self.assertEqual(response.json(),{'message':'NOT_FOUND_ROOM_ID'})
+        self.assertEqual(response.status_code, 400)
--- room/views.py
+++ room/views.py
@@ -1,2 +1,45 @@
-from django.db   import models
-from room.models import room
+import json, re
+import statistics
+
+from django.http           import JsonResponse
+from django.views          import View
+from django.db.models      import Q, Avg
+from django.core.paginator import Paginator
+
+from room.models           import Room, Category, RoomAmenity, Image, Amenity, WishList, DisableDate, AbleTime
+from user.models           import Host, Review
+
+class RoomView(View):
+    def get(self,request, room_id):
+        try: 
+            room        = Room.objects.get(id=room_id)
+            rating_list = [field.name for field in Review._meta.get_fields() if field.name not in ['id','review_user','review_room','comment']]
+
+            room_detail = {
+                'room_name': room.name,
+                'address'  : room.city,
+                'price'    : room.price,
+                'room_type': room.category.name,
+                'image'    : [image.url for image in room.image.all()][0],
+                'is_super' : room.host.is_super,
+                'host'     : room.host.user.last_name + room.host.user.first_name,
+                'capacity' : room.capacity,
+                'amenity'  : [{
+                    'id'         : roomamenity.amenity.id,
+                    'icon'       : re.sub('<i class=\\"|\\"></i>', '',roomamenity.amenity.image),
+                    'description': roomamenity.amenity.name
+                } for roomamenity in room.roomamenity_set.all()
+                ],
+                'rating'   : [{
+                    'category'       : category,
+                    'category_rating': int(Review.objects.filter(review_room=room).aggregate(Avg(category)).get(category+'__avg'))
+                } for category in rating_list
+                ]
+            }
+            
+            return JsonResponse({'detail': room_detail}, status=200)
+       
+        except KeyError:
+            return JsonResponse({'message':'KeyError'}, status=400)
+        except Room.DoesNotExist:
+            return JsonResponse({'message':'NOT_FOUND_ROOM_ID'}, status=400)
