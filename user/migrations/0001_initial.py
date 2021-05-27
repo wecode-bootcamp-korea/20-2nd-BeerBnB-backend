a16ff4b3a1f48bb55343b36292249e6f80666d4e
Action2theFuture <dnstks0204@google.com>
2021-05-27 22:50:04 +0900

    commit

--- user/migrations/0001_initial.py
+++ user/migrations/0001_initial.py
@@ -35,7 +35,11 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=128, null=True, unique=True)),
+<<<<<<< HEAD
                 ('social_id', models.CharField(max_length=128, null=True, unique=True)),
+=======
+                ('social_id', models.CharField(max_length=200, unique=True)),
+>>>>>>> e140d10... commit
                 ('first_name', models.CharField(max_length=32, null=True)),
                 ('last_name', models.CharField(max_length=32, null=True)),
                 ('password', models.CharField(max_length=128, null=True)),
