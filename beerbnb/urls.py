03f3b4ab6228af4c60426075fd7c5ccf7ca8c488
Action2theFuture <dnstks0204@google.com>
2021-05-27 14:55:08 +0900

    dev

--- beerbnb/urls.py
+++ beerbnb/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 
 urlpatterns = [
+<<<<<<< HEAD
     path('user', include('user.urls'))
+=======
+    path('user', include('user.urls')),
+>>>>>>> d5fa9d3... social login
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,36 @@
 from django.urls import path
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> e6d6d76... commit
+=======
+<<<<<<< HEAD
+>>>>>>> d5fa9d3... social login
 from user.views import Signup, Signin, KaKaoSignIn
+=======
+from user.views import Signup, Signin
+>>>>>>> 4d825b5... commit
 
 urlpatterns = [
     path('/signup', Signup.as_view()),
     path('/signin', Signin.as_view()),
     path('/kakao', KaKaoSignIn.as_view())
+<<<<<<< HEAD
+=======
+=======
+from user.views import Signin
+
+urlpatterns = [
+    path('/signin', Signin.as_view())
+>>>>>>> 7dc3f5a... commit
+=======
+from user.views import KaKaoSignIn
+
+urlpatterns = [
+    path('/kakao', KaKaoSignIn.as_view())
+>>>>>>> cd6ce12... social login
+>>>>>>> d5fa9d3... social login
 ]
