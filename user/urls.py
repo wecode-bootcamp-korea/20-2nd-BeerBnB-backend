8feeabe1075b062a59ff834d8585c274bd41d6c6
Action2theFuture <dnstks0204@google.com>
2021-05-29 19:37:02 +0900

    host

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user.views import Signup, Signin, KaKaoSignIn
+from user.views import Signup, Signin, KaKaoSignIn, HostView, HostUpload
 
 urlpatterns = [
     path('/signup', Signup.as_view()),
     path('/signin', Signin.as_view()),
-    path('/kakao', KaKaoSignIn.as_view())
+    path('/kakao', KaKaoSignIn.as_view()),
+    path('/host', HostView.as_view()),
+    path('/upload', HostUpload.as_view())
 ]
