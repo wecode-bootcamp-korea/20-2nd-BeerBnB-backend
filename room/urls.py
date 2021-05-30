27d4be5137e27c80bf0869df0c1732bebae698d6
Mac <mac@Macui-MacBook-Pro.local>
2021-05-30 20:05:40 +0900

    ADD: Roomfilter view

--- room/urls.py
+++ room/urls.py
@@ -1,7 +1,7 @@
 from django.urls    import path
-
-from .views         import RoomView
+from .views         import RoomView,RoomListView
 
 urlpatterns = [
     path('/<int:room_id>', RoomView.as_view()),
+    path('', RoomListView.as_view())
 ]
