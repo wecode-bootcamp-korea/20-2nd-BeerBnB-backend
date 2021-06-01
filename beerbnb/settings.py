6c1425496aa419375ef9c926b7eb3029f136279e
Action2theFuture <dnstks0204@google.com>
2021-06-01 20:40:18 +0900

    commit

--- beerbnb/settings.py
+++ beerbnb/settings.py
@@ -9,13 +9,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib     import Path
 from my_settings import DATABASES, SECRET_KEY, MY_AWS_ACCESS_KEY_ID, MY_AWS_SECRET_ACCESS_KEY, EMAIL
-<<<<<<< HEAD
-=======
-from my_settings import DATABASES, SECRET_KEY, MY_AWS_ACCESS_KEY_ID, MY_AWS_SECRET_ACCESS_KEY
-
->>>>>>> f5972cb... upload
-=======
->>>>>>> 0b6272a... commit
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
