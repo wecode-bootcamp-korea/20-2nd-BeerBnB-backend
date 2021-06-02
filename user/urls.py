aec0f6888bbb5332abf18e4901db68ac973b33dc
Action2theFuture <dnstks0204@google.com>
2021-06-02 13:48:39 +0900

    commit

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('/signin', Signin.as_view()),
     path('/kakao', KaKaoSignIn.as_view()),
     path('/upload', ProfileUpload.as_view()),
-    path('/auth/<str:uidb64>/<str:email_token>', Auth.as_view())
+    path('/auth/<str:uidb64>/<str:email_token>', Auth.as_view()),
     path('/host', HostView.as_view()),
     path('/hostupload', HostUpload.as_view())
 ]
