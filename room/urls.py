db788307f9ceb6dc67110d9010a7510f944fcbeb
Mac <mac@Macui-MacBook-Pro.local>
2021-06-03 20:50:34 +0900

    Add: room filter

--- room/urls.py
+++ room/urls.py
@@ -1,8 +1,9 @@
 from django.urls    import path
-from room.views         import WishListView, RoomView
+from .views         import WishListView, RoomView,RoomListView
 
 urlpatterns = [
     path('/<int:room_id>', RoomView.as_view()),
     path('/wishlist', WishListView.as_view()),
-    path('/wishlist/<int:room_id>', WishListView.as_view())
+    path('/wishlist/<int:room_id>', WishListView.as_view()),
+    path('', RoomListView.as_view())
 ]
